fe52c08d8def69afeb3bc82726c6aeeb75845e1e
KateTaneva <80552789+KateTaneva@users.noreply.github.com>
2021-11-18 14:40:38 +0200

    Add files via upload

---
+++ CustomerEmail.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react';
+
+// class CustomerEmail extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             data: {
+//                 username,
+//                 email,
+//             }
+//         };
+//     }
+
+//     componentDidMount(){
+
+//         const url = 'https://run.mocky.io/v3/09e642b5-b52f-43c1-837b-8ebf70c10813';
+//         fetch(url)
+//             .then(response => response.json())
+//             .then(() => this.setState([...data] ))
+//             console.log(data[1]);
+//     }
+// }
+
+// export default CustomerEmail;
+
+
---
+++ MoreInfoservice.js
@@ -0,0 +1,48 @@
+import ValidatorFunction from './Validator/Validator'
+
+const url = 'https://run.mocky.io/v3/09e642b5-b52f-43c1-837b-8ebf70c10813';
+
+async function getapi(url) {
+    const response = await fetch(url);
+
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        var data = await response.json();
+        console.log(data);
+        if (response) {
+            ValidatorFunction()
+        }else{
+            throw ('Username not found!')
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // export const getOne = ({user: name, email}) => {
+    //     // var customerName = category? `category=${category}` : "";
+
+    //     return fetch(url)
+    //     .then((results)=> results.json())
+    //     .then((data)=>{
+    //         var customerName = data[0]
+
+    //     })
+    //       .catch((error) => console.log(error))
+    // }
+
